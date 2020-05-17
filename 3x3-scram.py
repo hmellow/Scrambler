3156314aac36c95bdd264ae6859bfd583de11069
PiPinecone <58414056+pipinecone@users.noreply.github.com>
2020-05-16 22:21:49 -0600

    Working on 3x3 scramble.

---
+++ 3x3-scram.py
@@ -0,0 +1,98 @@
+from random import randint
+
+# Standard scramble: 17-25
+# Include double moves (Ex: R2, F2, etc.)
+# Exclude double layer moves (Ex: r, u, etc.)
+# Exclude rotational moves (x, y, z, etc.)
+# Exclude slice moves (M, E, S, etc.)
+# U(2)/U' = excl: U(2), U', D(2), D'
+# D(2)/D' = excl: U(2), U', D(2), D'
+# F(2)/F' = excl: F(2), F', B(2), B'
+# B(2)/B' = excl: F(2), F', B(2), B'
+# R(2)/R' = excl: R(2), R', L(2), L'
+# L(2)/L' = excl: R(2), R', L(2), L'
+# Moves: U, U', U2, D, D', D2, F, F', F2, B, B', B2, R, R', R2, L, L', L2
+# Separate moves using spaces only
+# Int length later by adding length parameter
+# In the future maybe allow moves like: (L, R)
+
+dupe_1 = ('U', "U'", 'U2', 'D', "D'", 'D2')
+dupe_2 = ('F', "F'", 'F2', 'B', "B'", 'B2')
+dupe_3 = ('R', "R'", 'R2', 'L', "L'", 'L2')
+scramble = []
+
+
+def scramble_3_integer():
+    for _ in range(1):
+        value = randint(0, 17)
+        return value
+# Generates random integer to determine directional move
+
+
+def scramble_3_assignment(integer):
+    if integer == 0:
+        character = 'U'
+        return character
+    elif integer == 1:
+        character = "U'"
+        return character
+    elif integer == 2:
+        character = 'U2'
+        return character
+    elif integer == 3:
+        character = 'D'
+        return character
+    elif integer == 4:
+        character = "D'"
+        return character
+    elif integer == 5:
+        character = 'D2'
+        return character
+    elif integer == 6:
+        character = 'F'
+        return character
+    elif integer == 7:
+        character = "F'"
+        return character
+    elif integer == 8:
+        character = 'F2'
+        return character
+    elif integer == 9:
+        character = 'B'
+        return character
+    elif integer == 10:
+        character = "B'"
+        return character
+    elif integer == 11:
+        character = 'B2'
+        return character
+    elif integer == 12:
+        character = 'R'
+        return character
+    elif integer == 13:
+        character = "R'"
+        return character
+    elif integer == 14:
+        character = 'R2'
+        return character
+    elif integer == 15:
+        character = 'L'
+        return character
+    elif integer == 16:
+        character = "L'"
+        return character
+    elif integer == 17:
+        character = 'L2'
+        return character
+# Assigns a character based on integer input
+
+# 1
+scramble.append(scramble_3_assignment(scramble_3_integer()))
+
+# 2
+scramble.append(scramble_3_assignment(scramble_3_integer()))
+while scramble[0] and scramble[1] in dupe_1 or dupe_2 or dupe_3:
+    scramble.insert(1, scramble_3_assignment(scramble_3_integer()))
+
+
+print(scramble)
