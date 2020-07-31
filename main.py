9e9d6cafd64f3a1ce4acc1fe710e2fe1eab80598
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-07-31 17:03:56 -0600

    2 commands

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 import discord
 from secrets import TOKEN
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix="*")
 
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(name="EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"))
-    reference_embed = discord.Embed(title="Up", color=0xADFF2F)
-    reference_embed.add_field(name="Action", value="Rotate top face 90 degrees clockwise.", inline=False)
-    reference_embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/738527822473068604/738821000011186296/U.png")
-    reference_embed.set_footer(text='Notation')
-    channel = client.get_channel(703071528882929748)
-    await channel.send(embed=reference_embed)
+
+
+# Notation u or just u
+# Find a way to make aliases
+@client.command()
+async def u(ctx: commands.Context):
+    up = discord.Embed(title="Up", color=0xADFF2F)
+    up.add_field(name="Action", value="Rotate top face 90 degrees clockwise.", inline=False)
+    up.set_thumbnail(url="https://cdn.discordapp.com/attachments/738890956912459777/738891020862881819/U-Notation-Colored.png")
+    up.set_footer(text='View from front face.')
+    await ctx.send(embed=up)
+
+
+@client.command()
+async def upprime(ctx: commands.Context):
+    upr = discord.Embed(title="Up Inverted", color=0xADFF2F)
+    upr.add_field(name="Action", value="Rotate top face 90 degrees anticlockwise.", inline=False)
+    upr.set_thumbnail(url="https://cdn.discordapp.com/attachments/738890956912459777/738891022636941394/U-Notation-Colored.png")
+    upr.set_footer(text='View from front face.')
+    await ctx.send(embed=upr)
+
 
 client.run(TOKEN)
