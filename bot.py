cfb40973da6674a01cbe7eb498712d20e57b6fff
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-07-30 18:00:15 -0600

    Finished token security.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from secrets import TOKEN
 
 client = discord.Client()
 
@@ -7,5 +8,6 @@ async def on_ready():
     print('Logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(name="Maintinence."))
 
-client.run('NzAzMDc5MzgzNDcyMjc1NTA4.XqJXsw.5TwV1Gd_oVAWxeMsYe2V7TqGbF8')
+
+client.run(TOKEN)
 
