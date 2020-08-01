bae7a6d1c658c8dc41334ddee6e8b0745493f541
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-07-31 23:06:04 -0600

    Invite command

--- cogs/info.py
+++ cogs/info.py
@@ -15,6 +15,15 @@ class Info(commands.Cog):
     async def ping(self, ctx: commands.Context):
         await ctx.send(f"Latency: **{round(self.client.latency * 1000)}ms**")
 
+    @commands.command(
+        name="invite",
+        description="Sends invite of the PineCube server.",
+        aliases=['Invite']
+    )
+    @commands.guild_only()
+    async def invite(self, ctx: commands.Context):
+        await ctx.send("**PineCube server invite:** https://discord.gg/55nrRkr")
+
 
 def setup(client: commands.Bot):
     client.add_cog(Info(client))
