dfb7b224372598dd0366355aa5953ab212c04a8c
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-07-31 18:25:11 -0600

    Aliases added.

--- cogs/notation.py
+++ cogs/notation.py
@@ -6,18 +6,26 @@ class notation(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
-    @commands.command()
+    @commands.command(
+        name='Up',
+        description='Notation command.',
+        aliases=['up', 'UP', 'uP', 'U']
+    )
     @commands.guild_only()
-    async def u(self, ctx: commands.Context):
+    async def up(self, ctx: commands.Context):
         up = discord.Embed(title="Up", color=0xADFF2F)
         up.add_field(name="Action", value="Rotate top face 90 degrees clockwise.", inline=False)
         up.set_thumbnail(url="https://cdn.discordapp.com/attachments/738890956912459777/738891020862881819/U-Notation-Colored.png")
         up.set_footer(text='View from front face.')
         await ctx.send(embed=up)
 
-    @commands.command()
+    @commands.command(
+        name="U'",
+        description='Notation command.',
+        aliases=[""]
+    )
     @commands.guild_only()
-    async def invertu(self, ctx: commands.Context):
+    async def upinverted(self, ctx: commands.Context):
         upr = discord.Embed(title="Up Inverted", color=0xADFF2F)
         upr.add_field(name="Action", value="Rotate top face 90 degrees anticlockwise.", inline=False)
         upr.set_thumbnail(
