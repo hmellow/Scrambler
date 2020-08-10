952033652d65b896c341eaee57fcd8190f88c59c
Mike968 <michaelhmehall@gmail.com>
2020-08-10 15:27:14 -0600

    Fixed scrambles.py

--- cogs/scrambles.py
+++ cogs/scrambles.py
@@ -13,7 +13,6 @@ class Scrambles(commands.Cog):
     )
     @commands.guild_only()
     async def scram3x3(self, ctx: commands.Context):
-
         scram = Scram({'moves': (('U', 'D'), ('F', 'B'), ('R', 'L')), 'directions': ('', "'", '2')}, 17, 25)
         scram.scram()
         scram.convert()
@@ -21,6 +20,28 @@ class Scrambles(commands.Cog):
         s3 = "**" + convert(scram.scramble) + "**"
 
         await ctx.send(s3)
+
+    @commands.command(
+        name="2x2",
+        aliases=['2', '2X2', '2x', '2X', 'Two', 'two', 'TWO']
+    )
+    @commands.guild_only()
+    async def scram2x2(self, ctx: commands.Context):
+        scram = Scram({'moves': (('U', 'D'), ('F', 'B'), ('R', 'L')), 'directions': ('', "'", '2')}, 9, 10)
+        scram.scram()
+        scram.convert()
+
+        s3 = "**" + convert(scram.scramble) + "**"
+
+        await ctx.send(s3)
+
+    @commands.command(
+        name="1x1",
+        aliases=['1', '1X1', '1x', '1X', 'One', 'one', 'ONE']
+    )
+    @commands.guild_only()
+    async def scr1(self, ctx: commands.Context):
+        await ctx.send("No. That's stupid.")
 
 
 def setup(client: commands.Bot):
