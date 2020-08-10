c9632f1a96f7ac44f88660c948196bdb01dda48b
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-08-09 23:12:04 -0600

    Class name change.

--- cogs/scrambles.py
+++ cogs/scrambles.py
@@ -2,10 +2,19 @@ import discord
 from discord.ext import commands
 
 
-class Scram3x3(commands.Cog):
+class Scrambles(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
 
 
+    @commands.command(
+        name="",
+        aliases=
+    )
+    @commands.guild_only()
+    async def scram3x3
+
+
+
 def setup(client: commands.Bot):
-    client.add_cog(Scram3x3(client))
+    client.add_cog(Scrambles(client))
