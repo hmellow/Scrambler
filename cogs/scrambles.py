978f0a253a7bfe84f4a2a2697a525eac525dbde0
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-08-10 13:05:25 -0600

    Moved out functions

--- cogs/scrambles.py
+++ cogs/scrambles.py
@@ -6,6 +6,37 @@ def convert(lizt):
     return ' '.join(lizt)
 
 
+def scram(small, large):
+    moves = (('U', 'D'), ('F', 'B'), ('R', 'L'))
+    directions = ('', "'", '2')
+    scramble_length = random.randint(small, large)
+
+    def group_find(move):
+        temp = moves[0]
+        if move[0] in temp:
+            return moves[0]
+        else:
+            temp = moves[1]
+            if move[0] in temp:
+                return moves[1]
+            else:
+                return moves[2]
+
+    scramble = []
+    for i in range(scramble_length):
+        if i == 0:
+            scramble.append(random.choice(random.choice(moves)) + random.choice(directions))
+        else:
+            while True:
+                move = random.choice(moves)
+                if move != group_find(scramble[i - 1]):
+                    scramble.append(random.choice(move) + random.choice(directions))
+                    break
+
+    s3 = "**" + convert(scramble) + "**"
+    return s3
+
+
 class Scrambles(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
@@ -16,35 +47,8 @@ class Scrambles(commands.Cog):
     )
     @commands.guild_only()
     async def scram3x3(self, ctx: commands.Context):
-        moves = (('U', 'D'), ('F', 'B'), ('R', 'L'))
-        directions = ('', "'", '2')
-        scramble_length = random.randint(17, 25)
 
-        def group_find(move):
-            temp = moves[0]
-            if move[0] in temp:
-                return moves[0]
-            else:
-                temp = moves[1]
-                if move[0] in temp:
-                    return moves[1]
-                else:
-                    return moves[2]
-
-        scramble = []
-        for i in range(scramble_length):
-            if i == 0:
-                scramble.append(random.choice(random.choice(moves)) + random.choice(directions))
-            else:
-                while True:
-                    move = random.choice(moves)
-                    if move != group_find(scramble[i - 1]):
-                        scramble.append(random.choice(move) + random.choice(directions))
-                        break
-
-        s3 = "**" + convert(scramble) + "**"
-
-        await ctx.send(s3)
+        await ctx.send(scram(17, 25))
 
 
 def setup(client: commands.Bot):
