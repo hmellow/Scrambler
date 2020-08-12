6abe0190dbaa9dcae5e535f81bef469264fb32f8
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2020-08-12 13:31:12 -0600

    Comment grammar

--- scrambles/scram_components.py
+++ scrambles/scram_components.py
@@ -1,11 +1,11 @@
-# Normals is for all puzzles were all 3 sides are the same
+# Normals are for all puzzles where all 3 sides are the same
 normals = {
     2:{'moves': (('U', 'D'), ('F', 'B'), ('R', 'L')), 'directions': ('', "'", '2')},
     3:{'moves': (('U', 'D'), ('F', 'B'), ('R', 'L')), 'directions': ('', "'", '2')},
     4:{'moves': (('U', 'D'), ('F', 'B'), ('R', 'L')), 'directions': ('', "'", '2', 'w2', 'w', "w'")}
 }
 
-# Irregulars are for puzzles with different sized sizes. Use tuples as the key according to (sizeX, sizeY, sizeZ)
+# Irregulars are for puzzles with different sized sides. Use tuples as the key according to (sizeX, sizeY, sizeZ)
 irregulars = {
     None
 }
